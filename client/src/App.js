60f90f2660f9fea213fc6919f0c071172d80387d
danielalvarezmejia <daniel.alvarez.mejia98@gmail.com>
2023-05-29 16:30:39 -0500

    front

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { Detail, Form, Home, Landing } from './Views';
+import NavBar from './Components/NavBar/NavBar';
+import { useEffect } from 'react';
+import style from './App.module.css'
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      document.body.classList.add(style.landing);
+    } else {
+      document.body.classList.remove(style.landing);
+    }
+  }, [location.pathname])
+
   return (
-    <div className="App">
-      <h1>Henry Videogames</h1>
+    <div className={style.container}>
+      { location.pathname !== '/' && <NavBar /> }
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/detail' element={<Detail />} />
+        <Route path='/form' element={<Form />} />
+      </Routes>
     </div>
   );
 }
